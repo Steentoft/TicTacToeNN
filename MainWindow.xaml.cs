81ad32c3898f8a107087b50097b9c66ff0bfa4ad
Christian Steentoft <chri32b0@edu.sde.dk>
2020-07-28 08:24:14 +0200

    0.1.0

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TicTacToeNN
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        float[][] Course;
+        int[] layer = new int[6] { 9, 20, 16, 14, 12, 9 };
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeCourse();
+
+        }
+
+        void StartTraining()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+
+
+
+
+            }
+        }
+
+        void InitializeCourse()
+        {
+            List<float[]> row = new List<float[]>();
+            for (int i = 0; i < 3; i++)
+            {
+                row.Add(new float[3]);
+            }
+            Course = row.ToArray();
+        }
+        void RestartCourse()
+        {
+            for (int i = 0; i < Course.Length; i++)
+            {
+                for (int j = 0; j < Course[i].Length; j++)
+                {
+                    Course[i][j] = 0;
+                }
+            }
+        }
+    }
+}
---
+++ ONeuralNetwork.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media;
+
+namespace ONeuralNetwork
+{
+    //By Christian Steentoft
+    public class ONeuralNetwork
+    {
+        int[] layers;
+        float[][] neurons;
+        float[][][] weights;
+
+        float fitness;
+        public ONeuralNetwork(int[] layers)
+        {
+            this.layers = layers;
+
+            InitializeNeurons();
+            InitializeFirstWeights();
+        }
+
+        void InitializeNeurons()
+        {
+            List<float[]> neuronRow = new List<float[]>();
+            for (int i = 0; i < layers.Length; i++)
+            {
+                neuronRow.Add(new float[layers[i]]);
+            }
+            neurons = neuronRow.ToArray();
+        }
+        void InitializeFirstWeights()
+        {
+            //OuterList
+            List<float[][]> weightList = new List<float[][]>();
+            for (int i = 1; i < layers.Length; i++)
+            {
+                //MiddleList
+                List<float[]> layerWeightList = new List<float[]>();
+                int neuronInLastLayer = layers[i - 1];
+                for (int j = 0; j < neurons[i].Length; j++)
+                {
+                    //InnerWeights
+                    float[] neuronWeights = new float[neuronInLastLayer];
+                    for (int k = 0; k < neuronInLastLayer; k++)
+                    {
+                        neuronWeights[k] = (float)GetRandomNumber(-0.5, 0.5);
+                    }
+                    //Adds InnerWeights to MiddleList
+                    layerWeightList.Add(neuronWeights);
+
+                }
+                //Adds MiddleList to OuterList
+                weightList.Add(layerWeightList.ToArray());
+
+            }
+            //Adds OuterList to actual weights
+            weights = weightList.ToArray();
+
+        }
+        public float[] FeedForward(float[] inputs)
+        {
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                neurons[0][i] = inputs[i];
+            }
+
+            for (int i = 1; i < layers.Length; i++)
+            {
+                for (int j = 0; j < neurons[i].Length; j++)
+                {
+                    float value = 0f;
+
+                    for (int k = 0; k < neurons[i - 1].Length; k++)
+                    {
+                        value += weights[i - 1][j][k] * neurons[i - 1][k];
+                    }
+
+                    neurons[i][j] = (float)Math.Tanh(value);
+                }
+            }
+
+            return neurons[neurons.Length - 1];
+        }
+        private double GetRandomNumber(double minimum, double maximum)
+        {
+            Random random = new Random();
+            return random.NextDouble() * (maximum - minimum) + minimum;
+        }
+
+        public float GetFitness()
+        {
+            return fitness;
+        }
+        public void AddFitness(float score)
+        {
+            fitness += score;
+        }
+
+        public void Mutate()
+        {
+            Random rand = new Random();
+            for (int i = 0; i < weights.Length; i++)
+            {
+                for (int j = 0; j < weights[i].Length; j++)
+                {
+                    for (int k = 0; k < weights[i][j].Length; k++)
+                    {
+                        int value = rand.Next(0, 100);
+                        if (value < 10)
+                        {
+                            weights[i][j][k] = weights[i][j][k] * (float)GetRandomNumber(-0.50, 0.50);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
